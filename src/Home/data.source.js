6bb8e4ce1b0f5ff00b478f4924af2b5ff65eb8f6
wgceoqq <1585744423@qq.com>
2020-04-06 06:58:23 +0800

    first commit

---
+++ src/Home/data.source.js
@@ -0,0 +1,56 @@
+import React from 'react';
+export const Banner10DataSource = {
+  wrapper: { className: 'banner1' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg0' },
+        title: {
+          className: 'banner1-title',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+        },
+        content: {
+          className: 'banner1-content',
+          children: '一个高效的页面动画解决方案',
+        },
+        button: { className: 'banner1-button', children: 'Learn More' },
+      },
+      {
+        name: 'elem1',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg1' },
+        title: {
+          className: 'banner1-title',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+        },
+        content: {
+          className: 'banner1-content',
+          children: '一个高效的页面动画解决方案',
+        },
+        button: { className: 'banner1-button', children: 'Learn More' },
+      },
+      {
+        name: 'elem2',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg1' },
+        title: {
+          className: 'banner1-title',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+        },
+        content: {
+          className: 'banner1-content',
+          children: '一个高效的页面动画解决方案',
+        },
+        button: { className: 'banner1-button', children: 'Learn More' },
+      },
+    ],
+  },
+};
